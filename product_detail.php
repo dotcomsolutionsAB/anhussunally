6b6d1d03a5e1b312b3feebdde3dd25e1b9a2e135
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-11-11 15:42:45 +0530

    A28

--- product_detail.php
+++ product_detail.php
@@ -169,7 +169,7 @@
 	<span class="posted_in"><strong>Categories: </strong><a href="https://www.anhussunally.com/product-category/clamps/" rel="tag">Clamps</a>, <a href="https://www.anhussunally.com/product-category/clamps/pipe-clamps/" rel="tag">Pipe Clamps</a></span>
 	
 	
-</div>
+</div><br>
             <p class="bottom30">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
             <!-- <ul class="review_list marginbottom15">
               <li><img src="images/star.png" alt="star">

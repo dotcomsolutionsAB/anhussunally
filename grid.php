34312ff707f04e28919885d9852b17b0e54342fe
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-11-12 16:52:45 +0530

    A83

--- grid.php
+++ grid.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-<title>Eren | grid</title>
+<title>A N Hussunally & Co</title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/animate.min.css">
@@ -29,17 +29,17 @@
 <body>
 
 <!--Loader-->
-<div class="loader">
+<!-- <div class="loader">
   <div class="spinner-load">
     <div class="dot1"></div>
     <div class="dot2"></div>
   </div>
-</div>
+</div> -->
 
 
 
 <!--TOPBAR-->
-<div class="topbar">
+<!-- <div class="topbar">
   <div class="container">
     <div class="row">
       <div class="col-sm-4">
@@ -72,132 +72,15 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 <!--HEADER-->
-<header>
-  <nav class="navbar navbar-default navbar-sticky bootsnav">
-    <div class="container">
-    
-      <div class="attr-nav">
-        <ul>
-          <li class="cart-toggler">
-            <a href="#.">
-            <i class="fa fa-shopping-cart"></i>
-            <span class="badge">3</span>
-            </a>
-            </li>
-          <li class="search"><a href="#"><i class="fa fa-search"></i></a></li>
-        </ul>
-      </div>
-    
-    <!-- Start Header Navigation -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-        <i class="fa fa-bars"></i>
-        </button>
-        <a class="navbar-brand" href="index.html"><img src="images/logo.png" class="logo" alt=""></a>
-      </div><!-- End Header Navigation -->
-    
-      <div class="collapse navbar-collapse" id="navbar-menu">
-        <ul class="nav navbar-nav navbar-right" data-in="fadeIn" data-out="fadeOut">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Home</a>
-            <ul class="dropdown-menu">
-              <li><a href="index.html">Home V1</a></li>
-              <li><a href="index2.html">Home V2</a></li>
-              <li><a href="index3.html">Home V3</a></li>
-            </ul>
-          </li>
-          <li class="dropdown active">
-            <a href="#." class="dropdown-toggle" data-toggle="dropdown">Products</a>
-            <ul class="dropdown-menu">
-              <li><a href="grid.html">Grid Default</a></li>
-              <li><a href="grid_list.html">Grid Lists</a></li>
-              <li><a href="grid_sidebar.html">Grid Sidebar</a></li>
-              <li><a href="list_sidebar.html">Lists Sidebar</a></li>
-            </ul>
-          </li>
-          <li><a href="#">collection</a></li>
-          <li class="dropdown megamenu-fw">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">pages</a>
-            <ul class="dropdown-menu megamenu-content" role="menu">
-              <li>
-                <div class="row">
-                  <div class="col-menu col-md-3">
-                    <h5 class="title heading_border">Blog</h5>
-                    <div class="content">
-                      <ul class="menu-col">
-                        <li><a href="blog1.html">Blog Two Cols</a></li>
-                        <li><a href="blog2.html">Blog Three Cols</a></li>
-                        <li><a href="blog_post.html">Blog Posts</a></li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-menu col-md-3">
-                    <h5 class="title heading_border">Products Elements</h5>
-                    <div class="content">
-                      <ul class="menu-col">
-                        <li><a href="checkout.html">Product Chekouts</a></li>
-                        <li><a href="product_detail.html">Products Details</a></li>
-                        <li><a href="cart.html">Shopping Cart</a></li> 
-                      </ul>
-                    </div>
-                  </div>
-                  <div class="col-menu col-md-3">
-                    <h5 class="title heading_border">Theme Elements</h5>
-                    <div class="content">
-                      <ul class="menu-col">
-                        <li><a href="#">Skills</a></li>
-                        <li><a href="#">Team & Testimonials</a></li>
-                        <li><a href="404.html">Errors</a></li>
-                      </ul>
-                    </div>
-                  </div>    
-                  <div class="col-menu col-md-3">
-                    <div class="content">
-                      <img src="images/mega-menu.png"  alt="menu" class="img-responsive">
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </li>
-          <li><a href="#.">about us</a></li>
-          <li><a href="contact.html">contact us</a></li>
-        </ul>
-      </div>
-      <div class="search-toggle">
-        <div class="top-search">
-          <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search">
-            <span class="input-group-addon"><i class="fa fa-search"></i></span>
-          </div>
-        </div>
-      </div>
-      <ul class="cart-list">
-    <li>
-      <a href="#." class="photo"><img src="images/hover-cart.jpg" class="cart-thumb" alt="" /></a>
-      <h6><a href="#.">Sacrificial Chair Design </a></h6>
-      <p>Qty: 2 <span class="price">$170.00</span></p>
-    </li>
-    <li class="total clearfix">
-      <div class="pull-right"><strong>Shipping</strong>: $5.00</div>
-      <div class="pull-left"><strong>Total</strong>: $173.00</div>
-      </li>
-      <li class="cart-btn">
-      <a href="#." class="active">VIEW CART </a>
-      <a href="#.">CHECKOUT </a>
-    </li>
-  </ul>
-    </div>   
-  </nav>
-</header>
+<?php include("inc_files/header.php"); ?>
 
 
 
 <!--Page Header-->
-<section class="header_layout2 padding">
+<!-- <section class="header_layout2 padding">
   <div class="container">
     <div class="header_content padding">
       <div class="row">
@@ -209,7 +92,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
 
 
 <!--Page Nav-->

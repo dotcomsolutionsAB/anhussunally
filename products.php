c16b34e662be21ec93af645b96240edd2e8a8022
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-11-14 11:52:32 +0530

    s53

--- products.php
+++ products.php
@@ -59,6 +59,52 @@
   <!--HEADER-->
   <?php include("inc_files/header.php");?>
 
+    <?php
+        // Determine the current page or category dynamically
+        $current_page = basename($_SERVER['PHP_SELF'], ".php"); // Gets the current PHP file name without extension
+
+        // Example logic for generating breadcrumb items
+        $breadcrumb_items = [];
+        $breadcrumb_items[] = ['name' => 'Home', 'link' => 'index.html'];
+
+        if ($current_page === 'products') {
+            $breadcrumb_items[] = ['name' => 'Products', 'link' => 'products.php'];
+
+            // If there is a specific product category or name, you can add it here
+            if (isset($_GET['category'])) {
+                $breadcrumb_items[] = ['name' => htmlspecialchars($_GET['category']), 'link' => '#'];
+            } elseif (isset($_GET['sku'])) {
+                $breadcrumb_items[] = ['name' => 'Product Details', 'link' => '#'];
+            }
+        } else {
+            $breadcrumb_items[] = ['name' => ucfirst($current_page), 'link' => '#'];
+        }
+    ?>
+
+    <section class="page_menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="hidden">hidden</h3>
+                    <ul class="breadcrumb">
+                        <?php foreach ($breadcrumb_items as $index => $item): ?>
+                            <li <?php echo $index === count($breadcrumb_items) - 1 ? 'class="active"' : ''; ?>>
+                                <?php if ($index !== count($breadcrumb_items) - 1): ?>
+                                    <a href="<?php echo htmlspecialchars($item['link']); ?>">
+                                        <?php echo htmlspecialchars($item['name']); ?>
+                                    </a>
+                                <?php else: ?>
+                                    <?php echo htmlspecialchars($item['name']); ?>
+                                <?php endif; ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
   <section id="feature_product" class="bottom_half">
     <div class="container">
       <div class="row">

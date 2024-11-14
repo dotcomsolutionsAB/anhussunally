d0af230d2d433cc0a370c1021fd70f149b50fc17
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-11-14 14:36:28 +0530

    s67

--- product_detail.php
+++ product_detail.php
@@ -182,11 +182,11 @@
                 }
             </style>
             <div class="cc" style="display:flex; flex-direction:row; justify-content:space-evenly;">
-              <a href="images/pdf.png" download=""><span style="width: 22px;">📑</span>
+              <a href="images/pdf.png" download="" style="background: #fb163e; padding: 5px 10px; border-radius: 8px; margin: 5px;"><span style="width: 22px;">📑</span>
                 <!-- <img class="brochure-pdf" src="images/pdf.png" alt="pdf" style="max-width:160px"> -->
                 <span style="color:white; font-weight:bold;">PDF</span>
               </a>
-              <a href="mailto:your-email@gmail.com" style="background: black; padding: 5px 10px; border-radius: 8px; margin: 5px;">
+              <a href="mailto:your-email@gmail.com" style="background: #262424; padding: 5px 10px; border-radius: 8px; margin: 5px;">
                   <img src="images/gmail.png" alt="mail"style="width: 22px;">
                   <span style="color:white; font-weight:bold;">Send Email</span>
               </a>

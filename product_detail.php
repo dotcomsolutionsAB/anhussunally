faadb11c707df9a577ea00e02bec8471e96439c9
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-11-16 13:20:49 +0530

    B22

--- product_detail.php
+++ product_detail.php
@@ -405,7 +405,7 @@ error_reporting(E_ALL);
         </div>
         <?php while ($relatedProduct = $relatedProductsResult->fetch_assoc()): ?>
             <div class="col-md-3 col-sm-6">
-                <div class="product_wrap bottom_half" style="padding-bottom: 0px; padding: 5px; border-radius: 20px;">
+                <div class="product_wrap bottom_half" style="padding: 5px; border-radius: 20px; margin-bottom: 5px; box-shadow: -1px 4px 19px -9px rgba(0, 0, 0, 0.5); background-color: white">
                     <?php if ($relatedProduct['hours_since_creation'] <= 24): ?>
                         <div class="tag-btn"><span class="uppercase text-center" style="color:#7ab6c8;">New</span></div>
                     <?php endif; ?>

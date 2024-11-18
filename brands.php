f3c63da77919cf21f6d5f2eeef79ddcdfa269307
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-11-18 12:51:16 +0530

    B38

---
+++ brands.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Walvoil</title>
+    <style>
+        .container {
+            display: flex;
+            align-items: center;
+            padding: 20px;
+        }
+        .image {
+            flex: 1;
+            padding-right: 20px;
+        }
+        .image img {
+            width: 100%;
+            height: auto;
+            border-radius: 8px;
+        }
+        .text {
+            flex: 2;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="image">
+            <img src="https://via.placeholder.com/300" alt="Demo Image">
+        </div>
+        <div class="text">
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet accumsan tortor. Curabitur laoreet, lacus sit amet sodales vestibulum, lectus risus aliquam elit, at bibendum elit odio eu mi. Integer eu sapien quis felis consequat scelerisque. Nulla facilisi. Sed fringilla ipsum nec velit hendrerit, ut dictum lorem consequat.</p>
+        </div>
+    </div>
+</body>
+</html>

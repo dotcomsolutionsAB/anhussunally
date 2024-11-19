18d8db096f09bd4faea2a5ee843fa2cf7e2e100b
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-11-19 18:37:36 +0530

    s133

--- inc_files/new_arrival.php
+++ inc_files/new_arrival.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 text-center">
-          <h2 class="heading_space uppercase">new arrivals
+          <h2 class="heading_space uppercase">new arri
           </h2>
           <p class="bottom_half">Claritas est etiam processus dynamicus, qui sequitur.
           </p>
@@ -11,49 +11,64 @@
       </div>
       <div class="row">
         <div id="fourCol-slider" class="owl-carousel">
-          <div class="item">
-            <div class="product_wrap">
-              <div class="image">
-                <div class="social">
-                  <ul>
-                    <li>
-                      <a href="#.">
-                        <i class="fa fa-expand">
-                        </i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#.">
-                        <i class="fa fa-exchange">
-                        </i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#.">
-                        <i class="fa fa-heart-o">
-                        </i>
-                      </a>
-                    </li>
-                  </ul>
+        <?php include("api/db_connection.php"); ?>
+
+<?php
+// Establish database connection
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch unique brand IDs from the products table
+$brandQuery = "SELECT DISTINCT brand_id FROM products";
+$brandResult = $conn->query($brandQuery);
+
+if ($brandResult && $brandResult->num_rows > 0):
+    while ($brand = $brandResult->fetch_assoc()):
+        $brandId = $brand['brand_id'];
+
+        // Fetch 4 random products for each brand
+        $productQuery = "SELECT * FROM products WHERE brand_id = $brandId ORDER BY RAND() LIMIT 4";
+        $productResult = $conn->query($productQuery);
+
+        if ($productResult && $productResult->num_rows > 0):
+            while ($product = $productResult->fetch_assoc()):
+                // Define the image URL or use a default image if not available
+                $imageLink = !empty($product['images']) ? "api/uploads/assets/" . explode(',', $product['images'])[0] : "images/default.png";
+?>
+                <!-- HTML structure for each product -->
+                <div class="item">
+                    <div class="product_wrap">
+                        <div class="image">
+                            <div class="social">
+                                <!-- Add social icons or buttons if needed -->
+                            </div>
+                            <a class="fancybox" href="<?php echo htmlspecialchars($imageLink); ?>">
+                                <img src="<?php echo htmlspecialchars($imageLink); ?>" alt="Product" class="img-responsive">
+                            </a>
+                        </div>
+                        <div class="product_desc">
+                            <p><?php echo htmlspecialchars($product['name']); ?></p>
+                            <span class="price">
+                                <i class="fa fa-gbp"></i> <?php echo htmlspecialchars(number_format($product['price'], 2)); ?>
+                            </span>
+                            <a class="fancybox" href="<?php echo htmlspecialchars($imageLink); ?>" data-fancybox-group="gallery">
+                                <i class="fa fa-shopping-bag open"></i>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <a class="fancybox" href="images/product5.jpg">
-                  <img src="images/product5.jpg" alt="Product" class="img-responsive">
-                </a>
-              </div>
-              <div class="product_desc">
-                <p>Sacrificial Chair Design
-                </p>
-                <span class="price">
-                  <i class="fa fa-gbp">
-                  </i>170.00
-                </span>
-                <a class="fancybox" href="images/product5.jpg" data-fancybox-group="gallery">
-                  <i class="fa fa-shopping-bag open">
-                  </i>
-                </a>
-              </div>
-            </div>
-          </div>
+<?php
+            endwhile;
+        endif;
+    endwhile;
+else:
+    echo "No brands found.";
+endif;
+
+$conn->close();
+?>
           <div class="item">
             <div class="product_wrap">
               <div class="image">

49345c5874e262b0ca06eeda8a34bdb5592c0511
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-11-22 14:08:16 +0530

    154

---
+++ about_us.php
@@ -0,0 +1,288 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>Contact Us</title>
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+  <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+  <link rel="stylesheet" type="text/css" href="css/owl.transitions.css">
+  <link rel="stylesheet" type="text/css" href="css/cubeportfolio.min.css">
+  <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css">
+  <link rel="stylesheet" type="text/css" href="css/bootsnav.css">
+  <link rel="stylesheet" type="text/css" href="css/settings.css">
+  <link rel="stylesheet" type="text/css" href="css/loader.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <link rel="shortcut icon" href="images/favicon.png">
+
+
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+
+  <!--Loader-->
+  <!-- <div class="loader">
+    <div class="spinner-load">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
+    </div>
+  </div> -->
+
+
+  <!--TOPBAR-->
+  <!-- <div class="topbar">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="header-top-entry">
+            <div class="title">
+              ENGLISH<i class="fa fa-angle-down"></i>
+            </div>
+            <div class="list">
+              <a class="list-entry" href="#">English</a>
+              <a class="list-entry" href="#">Spanish</a>
+            </div>
+          </div>
+          <div class="header-top-entry">
+            <div class="title">
+              USD<i class="fa fa-angle-down"></i>
+            </div>
+            <div class="list">
+              <a class="list-entry" href="#">$ CAD</a>
+              <a class="list-entry" href="#">€ EUR</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-8">
+          <ul class="top_link">
+            <li><a href="#" class="uppercase">My Account</a>
+            </li>
+            <li><a href="#" class="uppercase">wishlish</a>
+            </li>
+            <li><a href="#" class="uppercase">checkout</a>
+            </li>
+            <li><a href="#" class="uppercase">login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+  <!--HEADER-->
+  <?php include("inc_files/header.php");?>
+
+
+  <!--Page Header-->
+  <section class="page_header padding">
+    <div class="container">
+      <div class="header_content padding">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1 class="uppercase"> Contact Us</h1>
+            <p>Celebrating 100 years of Trust</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="page_menu">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="hidden">hidden</h3>
+          <ul class="breadcrumb">
+            <li><a href="index.php">Home</a>
+            </li>
+            <li class="active">About us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <style>
+        .testimonial-section {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            background-color: #111;
+            height: 100%;
+        }
+
+        .testimonial-text {
+            flex: 1;
+            margin: 20px 70px;
+            width: 50vw;
+            height: 95vh;
+            justify-content: center;
+            display: flex;
+            flex-direction: column;
+        }
+
+        .testimonial-text h2 {
+            font-size: 2.5em;
+            margin-bottom: 20px;
+            font-weight: bold;
+            text-transform: uppercase;
+            color: #fff;
+        }
+
+        .testimonial-text p {
+            font-size: 1.1em;
+            line-height: 1.8;
+            margin-bottom: 30px;
+            color: #ccc;
+        }
+
+        .client-info {
+            text-align: center;
+        }
+
+        .client-info img {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            margin-bottom: 10px;
+        }
+
+        .client-info h4 {
+            margin: 0;
+            font-size: 1em;
+            text-transform: uppercase;
+            color: #fff;
+        }
+
+        .client-info span {
+            font-size: 0.9em;
+            color: #888;
+        }
+
+        .testimonial-image {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .testimonial-image img {
+            width: 100%;
+            height: 100%;
+            /* padding:1px 5px; */
+            /* border-radius: 10px; */
+        }
+    </style>
+    <section class="testimonial-section">
+        <div class="testimonial-text">
+            <h2>About Us</h2>
+            <p>
+                Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the    market leaders in providing customers with holistic hydraulic solutions.<br><br>
+
+                Established in 1917, the Company’s principal business philosophy is anchored on providing customers with world-clasproducts and solutions at cost-effective propositions. Today, the Company comprehensively caters to the needs of a vasspectrum of industries in the pneumatics, oil and gas, power, steel, cement, manufacturing and chemicals industrie<br><br>
+
+                With strong resident knowledge and knowhow of the dynamic hydraulics industry coupled with state-of-the-arinfrastructure and resource base, A.N. Hussunally & Co. has actively engaged in nation-building by participating isuch landmark Indian projects as the Tata Steel complex in Jamshedpur, Bhusan Power & Steel, Jindal Steel, ISSCO SteePlant etc.
+            </p>
+        </div>
+        <div class="testimonial-image">
+            <img src="images/about-1.jpg" alt="Feedback">
+        </div>
+    </section>
+
+  <!--Contact Us-->
+  <section id="contact" class="padding_top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h3 class="uppercase heading bottom30">Send us a message</h3>
+          <form class="contact-form padding_bottom">
+            <div class="row">
+              <div class="col-md-6 form-group">
+                <label for="exampleInputName2">Name</label>
+                <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+              </div>
+              <div class="col-md-6 form-group">
+                <label for="exampleInputEmail2">Mail</label>
+                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+              </div>
+              <div class="col-md-12 form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" placeholder="Write your message here..."></textarea>
+                <input type="submit" class="btn-form uppercase border-radius margintop40" value="send your message">
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div class="col-md-4">
+          <div class="contact_detail padding_bottom">
+            <h3 class="uppercase heading bottom30">Get Office Info.</h3>
+            <p class="bottom30">Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the market leaders in providing customers with holistic hydraulic solutions.</p>
+            <div class="address bottom30">
+              <i class="fa fa-map-marker"></i>
+              <h5 class="uppercase">Our Address</h5>
+              <p>Hussunally Buildings<br>
+                Post Box No. 68628,
+                Strand Road, Kolkata
+                West Bengal-700 001.</p>
+            </div>
+            <div class="address bottom30">
+              <i class="fa  fa-phone"></i>
+              <h5 class="uppercase">Phone Number</h5>
+              <p>+91-33-2230 4140</p>
+            </div>
+            <div class="address">
+              <i class="fa fa-envelope-o"></i>
+              <h5 class="uppercase">Email Address</h5>
+              <p>Email: <a href="mailto:hussunally@gmail.com.">hussunally@gmail.com</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="padding_bottom">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.0104580223724!2d88.34563767484305!3d22.57871217948732!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277bb65e7eed7%3A0x98eee9c0ded7dfe2!2sA.N.Hussunally%20%26%20Co.!5e0!3m2!1sen!2sin!4v1731402795801!5m2!1sen!2sin" width="2000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </section>
+
+
+  <!--Footer-->
+  <?php include("inc_files/footer.php");?>
+
+  <script src="js/jquery-2.2.3.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAOBKD6V47-g_3opmidcmFapb3kSNAR70U"></script>
+  <script src="js/gmap3.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/bootsnav.js"></script>
+  <script src="js/jquery.parallax-1.1.3.js"></script>
+  <script src="js/jquery.appear.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.cubeportfolio.min.js"></script>
+  <script src="js/jquery.fancybox.js"></script>
+  <script src="js/jquery.themepunch.tools.min.js"></script>
+  <script src="js/jquery.themepunch.revolution.min.js"></script>
+  <script src="js/revolution.extension.layeranimation.min.js"></script>
+  <script src="js/revolution.extension.navigation.min.js"></script>
+  <script src="js/revolution.extension.parallax.min.js"></script>
+  <script src="js/revolution.extension.slideanims.min.js"></script>
+  <script src="js/revolution.extension.video.min.js"></script>
+  <script src="js/kinetic.js"></script>
+  <script src="js/jquery.final-countdown.js"></script>
+  <script src="js/functions.js"></script>
+
+</body>
+
+</html>
--- inc_files/home_about.php
+++ inc_files/home_about.php
@@ -1,86 +1,86 @@
-    <style>
-        .testimonial-section {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            background-color: #111;
-            height: 100%;
-        }
+<style>
+    .testimonial-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #111;
+        height: 100%;
+    }
 
-        .testimonial-text {
-            flex: 1;
-            margin: 20px 70px;
-            width: 50vw;
-            height: 95vh;
-            justify-content: center;
-            display: flex;
-            flex-direction: column;
-        }
+    .testimonial-text {
+        flex: 1;
+        margin: 20px 70px;
+        width: 50vw;
+        height: 95vh;
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+    }
 
-        .testimonial-text h2 {
-            font-size: 2.5em;
-            margin-bottom: 20px;
-            font-weight: bold;
-            text-transform: uppercase;
-            color: #fff;
-        }
+    .testimonial-text h2 {
+        font-size: 2.5em;
+        margin-bottom: 20px;
+        font-weight: bold;
+        text-transform: uppercase;
+        color: #fff;
+    }
 
-        .testimonial-text p {
-            font-size: 1.1em;
-            line-height: 1.8;
-            margin-bottom: 30px;
-            color: #ccc;
-        }
+    .testimonial-text p {
+        font-size: 1.1em;
+        line-height: 1.8;
+        margin-bottom: 30px;
+        color: #ccc;
+    }
 
-        .client-info {
-            text-align: center;
-        }
+    .client-info {
+        text-align: center;
+    }
 
-        .client-info img {
-            width: 80px;
-            height: 80px;
-            border-radius: 50%;
-            margin-bottom: 10px;
-        }
+    .client-info img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        margin-bottom: 10px;
+    }
 
-        .client-info h4 {
-            margin: 0;
-            font-size: 1em;
-            text-transform: uppercase;
-            color: #fff;
-        }
+    .client-info h4 {
+        margin: 0;
+        font-size: 1em;
+        text-transform: uppercase;
+        color: #fff;
+    }
 
-        .client-info span {
-            font-size: 0.9em;
-            color: #888;
-        }
+    .client-info span {
+        font-size: 0.9em;
+        color: #888;
+    }
 
-        .testimonial-image {
-            flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
+    .testimonial-image {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
-        .testimonial-image img {
-            width: 100%;
-            height: 100%;
-            /* padding:1px 5px; */
-            /* border-radius: 10px; */
-        }
-    </style>
-        <section class="testimonial-section">
-            <div class="testimonial-text">
-                <h2>About Us</h2>
-                <p>
-                    Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the   market leaders in providing customers with holistic hydraulic solutions.<br><br>
+    .testimonial-image img {
+        width: 100%;
+        height: 100%;
+        /* padding:1px 5px; */
+        /* border-radius: 10px; */
+    }
+</style>
+<section class="testimonial-section">
+    <div class="testimonial-text">
+        <h2>About Us</h2>
+        <p>
+            Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the    market leaders in providing customers with holistic hydraulic solutions.<br><br>
 
-                        Established in 1917, the Company’s principal business philosophy is anchored on providing customers with world-class products and solutions at cost-effective propositions. Today, the Company comprehensively caters to the needs of a vast spectrum of industries in the pneumatics, oil and gas, power, steel, cement, manufacturing and chemicals industries.<br><br>
+            Established in 1917, the Company’s principal business philosophy is anchored on providing customers with world-clasproducts and solutions at cost-effective propositions. Today, the Company comprehensively caters to the needs of a vasspectrum of industries in the pneumatics, oil and gas, power, steel, cement, manufacturing and chemicals industrie<br><br>
 
-                        With strong resident knowledge and knowhow of the dynamic hydraulics industry coupled with state-of-the-art infrastructure and resource base, A.N. Hussunally & Co. has actively engaged in nation-building by participating in such landmark Indian projects as the Tata Steel complex in Jamshedpur, Bhusan Power & Steel, Jindal Steel, ISSCO Steel Plant etc.
-                </p>
-            </div>
-            <div class="testimonial-image">
-                <img src="images/about-1.jpg" alt="Feedback">
-            </div>
-        </section>
+            With strong resident knowledge and knowhow of the dynamic hydraulics industry coupled with state-of-the-arinfrastructure and resource base, A.N. Hussunally & Co. has actively engaged in nation-building by participating isuch landmark Indian projects as the Tata Steel complex in Jamshedpur, Bhusan Power & Steel, Jindal Steel, ISSCO SteePlant etc.
+        </p>
+    </div>
+    <div class="testimonial-image">
+        <img src="images/about-1.jpg" alt="Feedback">
+    </div>
+</section>

a272ecacfcb333ecdfeac7625ce8c11c17a6e331
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-06 15:44:25 +0530

    A544

--- contact.php
+++ contact.php
@@ -130,7 +130,7 @@
                     <div class="col-lg-8">
                         <!-- contact-map -->
                         <div class="contact-map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48409.69813174607!2d-74.05163325136718!3d40.68264649999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25bae694479a3%3A0xb9949385da52e69e!2sBarclays%20Center!5e0!3m2!1sen!2sbd!4v1684309529719!5m2!1sen!2sbd" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.010458022371!2d88.34563767535545!3d22.578712179487383!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277bb65e7eed7%3A0x98eee9c0ded7dfe2!2sA.N.Hussunally%20%26%20Co.!5e0!3m2!1sen!2sin!4v1733480034069!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                         <!-- contact-map-end -->
                         <div class="contact__form-wrap">

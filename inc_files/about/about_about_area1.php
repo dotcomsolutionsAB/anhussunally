9bbba86476435cdc5a7beb5ff5264332d6adfe03
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-06 15:05:25 +0530

    A523

--- inc_files/about/about_about_area1.php
+++ inc_files/about/about_about_area1.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="row align-items-center justify-content-center">
             <div class="col-lg-6 col-md-10">
-                <div class="about__img-wrap-two">
+                <!-- <div class="about__img-wrap-two">
                     <img src="	https://anh.ongoingwp.xyz/images/about-1.jpg" alt="img" class="wow img-custom-anim-right animated" data-wow-duration="1.5s" data-wow-delay="0.2s">
                     <img src="https://anh.ongoingwp.xyz/images/imggg2.jpg" alt="img" class="wow img-custom-anim-left animated" data-wow-duration="1.5s" data-wow-delay="0.4s">
                     <img src="	https://anh.ongoingwp.xyz/images/imggg3.jpg" alt="img" class="wow img-custom-anim-top animated" data-wow-duration="1.5s" data-wow-delay="0.6s">
-                </div>
+                </div> -->
             </div>
             <div class="col-lg-6">
                 <div class="about__content-two">
@@ -14,11 +14,11 @@
                         <span class="sub-title">AN Hussunally & Co</span>
                         <h2 class="title">About Us</h2>
                     </div>
-                    <h5>Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the market leaders in providing customers with holistic hydraulic solutions.<br><br>
+                    <p>Pioneering the hydraulics market in Eastern India, the 100-year old A.N. Hussunally & Co. has today emerged as among the market leaders in providing customers with holistic hydraulic solutions.<br><br>
 
                         Established in 1917, the Company’s principal business philosophy is anchored on providing customers with world-class products and solutions at cost-effective propositions. Today, the Company comprehensively caters to the needs of a vass spectrum of industries in the pneumatics, oil and gas, power, steel, cement, manufacturing and chemicals industries.<br><br>
 
-                        With strong resident knowledge and knowhow of the dynamic hydraulics industry coupled with state-of-the-art infrastructure and resource base, A.N. Hussunally & Co. has actively engaged in nation-building by participating in such landmark Indian projects as the Tata Steel complex in Jamshedpur, Bhusan Power & Steel, Jindal Steel, ISSCO Steel Plant etc.</h5>
+                        With strong resident knowledge and knowhow of the dynamic hydraulics industry coupled with state-of-the-art infrastructure and resource base, A.N. Hussunally & Co. has actively engaged in nation-building by participating in such landmark Indian projects as the Tata Steel complex in Jamshedpur, Bhusan Power & Steel, Jindal Steel, ISSCO Steel Plant etc.</p>
                     <!-- <div class="experience__wrap">
                         <div class="experience__box">
                             <h3 class="title">25+</h3>

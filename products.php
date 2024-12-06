bf15caf65141aeb537912d3583355dbb3ccc1860
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-06 17:16:34 +0530

    A554

--- products.php
+++ products.php
@@ -107,13 +107,13 @@ if (isset($_GET['category_id'])) {
 </head>
 <body>
     <!--Preloader-->
-    <div id="preloader">
+    <!-- <div id="preloader">
         <div id="loader" class="loader">
             <div class="loader-container">
                 <div class="loader-icon"><img src="assets/img/logo/preloader.svg" alt="Preloader"></div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!--Preloader-end -->
 
     <!-- Scroll-top -->

491938609e9da8cdf3785862ab5269f6ecc74110
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-06 15:00:35 +0530

    A519

--- index.php
+++ index.php
@@ -30,13 +30,13 @@
 <body>
 
     <!--Preloader-->
-    <div id="preloader">
+    <!-- <div id="preloader">
         <div id="loader" class="loader">
             <div class="loader-container">
                 <div class="loader-icon"><img src="assets/img/logo/preloader.svg" alt="Preloader"></div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!--Preloader-end -->
 
     <!-- Scroll-top -->

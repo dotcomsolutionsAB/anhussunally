2e097fca398cd6543d74a751a1c726d047465792
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-07 11:47:48 +0530

    s549

--- inc_files/slider_section.php
+++ inc_files/slider_section.php
@@ -21,7 +21,7 @@
                         }
 
                         .flex::before {
-                            /* content: ""; */
+                            content: "";
                             position: absolute;
                             top: 0;
                             left: 0;

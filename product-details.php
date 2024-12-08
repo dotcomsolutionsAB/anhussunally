80f315a0afea3f132dc79b906bad64a74c23e2a2
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-08 20:40:32 +0530

    s588

--- product-details.php
+++ product-details.php
@@ -17,7 +17,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title><?php echo $product['name']; ?></title>
+    <title></title>
     <meta name="description" content="Renova - Construction Building & Renovation Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 

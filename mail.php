04549b099602c82857ba90ebf99e4ea7280654d7
Shaqlin Mondal <shaqlinmondal8905@gmail.com>
2024-12-13 12:48:56 +0530

    s631

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@
  $password = '9kCuzrb5tO53$xQtf';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {

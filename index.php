07d3b660724394a3ba482d5d56f008b03e0c2985
Aliasgar10 <aliasgarrampurawala2@gmail.com>
2024-12-19 12:16:09 +0530

    a647

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="AN Hussunally & Co">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/Favicon.png">
     <!-- Place favicon.ico in the root directory -->
 
     <!-- CSS here -->
